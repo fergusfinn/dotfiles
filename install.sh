c1bb92ea12fadf97cfb05fcccb4ec5520cce0e94
Fergus <fergus.barratt00@gmail.com>
2025-02-05 08:49:04 +0000

    remove lsd

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   brew install kubectl starship lsd fzf zsh
 else
   echo "Installing dependencies using apt..."
-  sudo apt update && sudo apt install -y lsd fzf zsh
+  sudo apt update && sudo apt install -y fzf zsh
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   chmod +x kubectl
   mkdir -p ~/.local/bin
--- zshrc
+++ zshrc
@@ -8,7 +8,6 @@ fi
 
 # Requires nvim, exa, kubectl, starship, zoxide, fzf
 alias vim="nvim"
-alias ls="exa"
 alias k="kubectl"
 alias awk="gawk"
 export EDITOR=nvim
